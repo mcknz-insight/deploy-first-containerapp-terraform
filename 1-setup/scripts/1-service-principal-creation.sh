f05f0721c0ea2d1078caa33c88ab3501044040b2
Jeff McKenzie <jmckenzi@starbucks.com>
2023-04-16 18:45:51 -0400

    corrected typo on setup az cli script: should be 'scope' not 'scopes'

--- 1-setup/scripts/1-service-principal-creation.sh
+++ 1-setup/scripts/1-service-principal-creation.sh
@@ -7,4 +7,4 @@ SUBSCRIPTION_ID=$(az account show --query id -o tsv)
 SP_NAME="firstContainerAppGitHubAction"
 az ad sp create-for-rbac --name $SP_NAME --role "contributor" --scopes "/subscriptions/$SUBSCRIPTION_ID" --sdk-auth  --output json
 servicePrincipalAppId=$(az ad sp list --display-name $SP_NAME --query "[].appId" -o tsv)
-az role assignment create --assignee $servicePrincipalAppId --role "User Access Administrator" --scopes "/subscriptions/$SUBSCRIPTION_ID"
+az role assignment create --assignee $servicePrincipalAppId --role "User Access Administrator" --scope "/subscriptions/$SUBSCRIPTION_ID"
