dcd541db1bd8e4c50c99c53679997082205a9a23
Jeff McKenzie <jmckenzi@starbucks.com>
2023-04-16 15:27:30 -0400

    updated az region and storage account name to be unique

--- 2-setup-terraform/scripts/1-create-terraform-storage.sh
+++ 2-setup-terraform/scripts/1-create-terraform-storage.sh
@@ -4,13 +4,13 @@
 # Creates the relevant storage account to store terraform state locally
 
 RESOURCE_GROUP_NAME="deploy-first-containerapp-rg"
-STORAGE_ACCOUNT_NAME="deployfirstcontainersa"
+STORAGE_ACCOUNT_NAME="deployfirstcontainer3239"
 
 # Create Resource Group
-az group create -l uksouth -n $RESOURCE_GROUP_NAME
+az group create -l eastus2 -n $RESOURCE_GROUP_NAME
 
 # Create Storage Account
-az storage account create -n $STORAGE_ACCOUNT_NAME -g $RESOURCE_GROUP_NAME -l uksouth --sku Standard_LRS
+az storage account create -n $STORAGE_ACCOUNT_NAME -g $RESOURCE_GROUP_NAME -l eastus2 --sku Standard_LRS
 
 # Create Storage Account blob
 az storage container create  --name tfstate --account-name $STORAGE_ACCOUNT_NAME
